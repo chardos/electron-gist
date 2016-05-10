ce490468ddb58892f8859a5f875857d4364a8035
Richard Tan <chardos@gmail.com>
2016-05-11 00:33:15 +1000

    outputting links

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 $(document).on('ready', function(){
+  getGists();
+})
+
+function getGists(){
   $.ajax({
     url: 'https://api.github.com/users/chardos/gists'
   })
   .done(function(data){
     $.each(data, function(i, gist){
       var gistObj = gist.files[Object.keys(gist.files)[0]]
-      console.log( gistObj  );
+      generateLink(gistObj.filename, gistObj.raw_url)
     })
   })
+}
 
-})
+function generateLink(name, url){
+  var $a = $('<a>' + name + '</a>')
+    .attr('href', url)
+    .addClass('GistLink');
+  $('body').append($a);
+}
