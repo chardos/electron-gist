dd95800d74de4c05ebf1856bbfa85d424aad9555
Richard Tan <chardos@gmail.com>
2016-05-12 08:39:52 +1000

    Remove header frame

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 400, height: 600})
+  mainWindow = new BrowserWindow({width: 400, height: 600, frame: false})
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
